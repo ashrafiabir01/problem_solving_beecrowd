567980ebdfcc19ece5ba2c9d1c3bb7ec6a238cab
ashrafiabir01 <ashrafiabir04@gmail.com>
2022-04-12 22:26:48 +0600

    first commit

---
+++ bee_1020_age_in_days.py
@@ -0,0 +1,9 @@
+inp = int(input())
+algorithom1 = inp%365
+year = int(inp/365)
+algorithom3 = algorithom1%30
+month = int(algorithom1/30)
+day = algorithom3
+print(f"{year} ano(s)")
+print(f"{month} mes(es)")
+print(f"{day} dia(s)")
